d4792b59941461a9981bfa6538eaf78f7f243797
oakarakum <onur.ardakarakum@outlook.com>
2022-11-18 10:33:06 +0300

    http hello

---
+++ lib/widgets/http_hello.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HttpHelloView extends StatefulWidget {
+  const HttpHelloView({super.key});
+
+  @override
+  State<HttpHelloView> createState() => _HttpHelloViewState();
+}
+
+class _HttpHelloViewState extends State<HttpHelloView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
