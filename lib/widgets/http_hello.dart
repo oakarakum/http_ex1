18baf641919c16e42bdba630d6deaaef5fa9ceb5
oakarakum <onur.ardakarakum@outlook.com>
2022-11-18 11:32:16 +0300

    http'yi yazıryorsun

--- lib/widgets/http_hello.dart
+++ lib/widgets/http_hello.dart
@@ -1,6 +1,7 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:http/http.dart';
 
 class HttpHelloView extends StatefulWidget {
   const HttpHelloView({super.key});
@@ -10,8 +11,38 @@ class HttpHelloView extends StatefulWidget {
 }
 
 class _HttpHelloViewState extends State<HttpHelloView> {
+  late bool isHaveData;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    isHaveData = false;
+  }
+
+  void getApiJohnName() {
+    return http
+        .get("https://hwasampleapi.firebaseio.com/api/books/0/author.json");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(toolbarHeight: 0),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            isHaveData
+                ? CircularProgressIndicator()
+                : Text(
+                    "Done ...",
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+            ElevatedButton(onPressed: () {}, child: Text("Send Request"))
+          ],
+        ),
+      ),
+    );
   }
 }
